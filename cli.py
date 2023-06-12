86be8063b4640c7d2759e392b5c0763d0461620b
ViktorTil <131162636+ViktorTil@users.noreply.github.com>
2023-06-12 21:22:00 +0300

    cli.py

--- cli.py
+++ cli.py
@@ -2,47 +2,57 @@ commands = ["hello", ["good bye", "close", "exit", "bye", "esc", "q"], "add", "c
 phone_book = {"Vasya": "0505436565", "Lesya" : "0674231111"}
 
 
+def error_with_func(arg):
+    def input_error(func):
+        def proc_error(*args):
+            try:
+                func(*args)
+            except IndexError:
+                print(f'Добавьте все данные в команду {arg}')
+            except ValueError:
+                pass
+        return proc_error
+    return input_error
+
+
+@error_with_func('add')  
 def add_phone(cont):
-    contact = cont.split(' ')
-    number_add= f'Введите все данные для добавления номера'
-    if len(contact) == 3:
-        
-        for phone in phone_book:
+    contact = cont.split(' ')        
+    for phone in phone_book:
             
-            if phone.casefold() == contact[1].casefold():
-                print(f'Контакт {contact[1]} уже есть в книге с номером {phone_book.get(phone)}')
-                return True
+        if phone.lower() == contact[1].lower():
+            print(f'Контакт {contact[1]} уже есть в книге с номером {phone_book.get(phone)}')
+            return True
                 
-        phone_book[contact[1]] = contact[2]   
-        number_add = f'ok'
+    phone_book[contact[1]] = contact[2]   
+    number_add = f'ok'
         
     print(number_add)
 
         
 
-
+@error_with_func('change')
 def change(contact):
     chng_cont = contact.split(' ')
-    number_chng= f'Введите все данные для изменения номера'
-    if len(chng_cont) == 3:
-        number_chng = f'Нет контакта {chng_cont[1]} в Вашем списке'
-        for phone in phone_book:
-            if phone.casefold() != chng_cont[1].casefold():
-                continue
-            else:
-                phone_book[phone] = chng_cont[2]
-                number_chng = f'Контакт {chng_cont[1]} изменен' 
+    number_chng = f'Нет контакта {chng_cont[1]} в Вашем списке'
+    
+    for phone in phone_book:
+        
+        if phone.lower() != chng_cont[1].lower():
+            continue
+        else:
+            phone_book[phone] = chng_cont[2]
+            number_chng = f'Контакт {chng_cont[1]} изменен' 
             
     print(number_chng)
 
+@error_with_func('phone')
 def phone(contact):
     c_number=contact.split(" ")
-    number = 'Введите контакт, который вы ищите'
-    if len(c_number) == 2:
-        number = f'В ваших контактах отсутствует {c_number[1]}'
-        for phone in phone_book:
-            if c_number[1].casefold() == phone.casefold():
-                number = f'{phone_book.get(phone)}'
+    number = f'В ваших контактах отсутствует {c_number[1]}'
+    for phone in phone_book:
+        if c_number[1].lower() == phone.lower():
+            number = f'{phone_book.get(phone)}'
     print(number)
     
     
@@ -51,7 +61,6 @@ def reply(command):
     operator=command.lower().split(" ")
     if command.lower() in commands[1]:
         print(answers[1])
-        print(phone_book)
         bot = False
     elif operator[0] in commands or command.lower() in commands:
         try:
@@ -85,5 +94,3 @@ def main():
         
 if __name__ == '__main__':
     main()
-
-
